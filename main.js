86c7130c98ddd5b202252ee67fe309b20c7153d2
EdAbdelrahman <abdelrahmansaeed1011@gmail.com>
2024-08-17 19:25:58 +0300

    finished HTML , Css files js file stile under developing

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let clear = document.querySelector('#clear-btn');
+    let equal = document.querySelector('.equal');
+    let decimal = document.querySelector('.decimal');
+
+    let numbers = document.querySelectorAll('.number');
+    let operator = document.querySelectorAll('.operator');
+
+    let previousScreen = document.querySelector('.previous');
+    let currentScreen = document.querySelector('.current');
+})
